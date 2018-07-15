39a8b93ea84dfb3480ec2527eb77d8933d38ba2a
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2018-07-15 15:46:06 +0100

    Adds play functionality to mancala game

--- mancala/mancala.py
+++ mancala/mancala.py
@@ -1,82 +1,58 @@
+import copy
+
 class Mancala():
 
     is_new = True
 
     def __init__(self, pots, stones):
+        play_board = [stones] * pots
+        self.game_log = [[play_board, play_board, [0,0]]]
 
-        self.number_of_pots = pots
-        self.number_of_stones = stones
-        self.stores = [0,0]
-        self.player1 = [stones] * pots
-        self.player2 = [stones] * pots
-        self.gameState = self.generateGameBoard(self)
+    def validate_turn(self, player_number, pot_number):
+        if player_number < 0 or player_number > 1:
+            raise ValueError("A valid player must be given")
 
-    def generateGameBoard(self):
+        try:
+            self.game_log[-1][player_number][pot_number]
+        except IndexError as error:
+            raise Exception("A valid pot must be selected for play") from error
 
-        return [self.player1, self.player2, self.stores]
+    def generate_turn(self, player_number, pot_number):
+        new_turn = copy.deepcopy(self.game_log[-1])
+        stones_to_sow = new_turn[player_number][pot_number]
+
+        new_turn[player_number][pot_number] = 0;
+
+        count = 1
+
+        while count <= stones_to_sow:
+            pot = pot_number + count
+            try:
+                new_turn[player_number][pot]
+            except IndexError:
+                print("out of bounds")
+            else:
+                print("pot exists " + str(pot))
+                new_turn[player_number][pot] += 1
+
+            count += 1
+
+        return new_turn
+
+    def play(self, player_number, pot_number):
+        player_number = player_number - 1;
+        pot_number = pot_number - 1;
+
+        self.validate_turn(player_number, pot_number)
+        self.game_log.append(self.generate_turn(player_number, pot_number))
 
 
-    # def condition(self, name):
-    #     """This function returns the condition of the Car instance. Googley
-    #     docstring.
-    #
-    #     Args:
-    #        name (str):  Some test argument.
-    #
-    #     Returns:
-    #        str.  The return code::
-    #
-    #           "New" -- The car is new.
-    #           "Used" -- The car is used.
-    #     """
-    #     if self.is_new:
-    #         return "New"
-    #     else:
-    #         return "Used"
-    #
-    # def drive(self, distance):
-    #     """This function drives the Car and adds to the mileage. This is a
-    #     numpydoc docstring.
-    #
-    #     Parameters
-    #     ----------
-    #     distance : int
-    #         This is the distance travelled by the car and will be added to the
-    #         current mileage.
-    #
-    #     Returns
-    #     -------
-    #     None
-    #
-    #     Raises
-    #     ------
-    #     BadException
-    #         Because you shouldn't have done that.
-    #
-    #     Notes
-    #     -----
-    #     Notes about the implementation algorithm (if needed).
-    #
-    #     This can have multiple paragraphs.
-    #
-    #     You may include some math:
-    #
-    #     .. math:: X(e^{j\omega } ) = x(n)e^{ - j\omega n}
-    #
-    #     And even use a greek symbol like :math:`omega` inline.
-    #
-    #     """
-    #     self.is_new = False
-    #     self.mileage += distance
-    #
-    # def description(self, name):
-    #     """This function prints the description of the car instance.
-    #
-    #     :param name: Some test argument.
-    #     :type name: str.
-    #     :returns:  str -- the return code.
-    #     :raises: AttributeError, KeyError
-    #
-    #     """
-    #     print("This %s was made in %s. It is %s and its condition is %s."
-    #           % (self.model, self.year, self.colour, self.condition('test')))
+
+
+
+
+
+
+
+
+
--- tests/test_mancala.py
+++ tests/test_mancala.py
@@ -1,35 +1,51 @@
 from mancala.mancala import Mancala
 import pytest
 
-class TestMancalaInit:
+class TestInitialGameState:
 
     @pytest.fixture(scope='function')
     def mancala(self):
         return Mancala(6, 4)
 
-    def test_has_starting_number_of_pots(self, mancala):
-        assert mancala.number_of_pots == 6
-
-    def test_has_starting_number_of_stones(self, mancala):
-        assert mancala.number_of_stones == 4
-
-    def test_has_starting_stores(self, mancala):
-        assert mancala.stores == [0,0]
-
-    def test_has_player_1_pots(self, mancala):
-        assert mancala.player1 == [4,4,4,4,4,4]
-
-    def test_has_player_2_pots(self, mancala):
-        assert mancala.player2 == [4,4,4,4,4,4]
-
-
-class TestGameState:
-
-    @pytest.fixture(scope='function')
-    def mancala(self):
-        return Mancala(6, 4)
-
-    def test_returns_game_state(self, mancala):
-        assert mancala.gameState == [
+    def test_returns_initial_game_log(self, mancala):
+        assert mancala.game_log == [
             [[4,4,4,4,4,4], [4,4,4,4,4,4], [0,0]]
         ]
+
+class TestGamePlay:
+
+    @pytest.fixture(scope='function')
+    def mancala(self):
+        return Mancala(6, 4)
+
+    def test_throws_error_when_player_number_is_too_high(self, mancala):
+        player_number = 3
+        pot_number = 4
+
+        with pytest.raises(ValueError) as error_message:
+            mancala.play(player_number, pot_number)
+        assert str(error_message.value) == "A valid player must be given"
+
+    def test_throws_error_when_player_number_is_too_low(self, mancala):
+        player_number = 0
+        pot_number = 2
+
+        with pytest.raises(ValueError) as error_message:
+            mancala.play(player_number, pot_number)
+        assert str(error_message.value) == "A valid player must be given"
+
+    def test_throws_error_when_pot_selected_does_not_exist(self, mancala):
+        player_number = 1
+        pot_number = 7
+
+        with pytest.raises(Exception) as error_message:
+            mancala.play(player_number, pot_number)
+        assert str(error_message.value) == "A valid pot must be selected for play"
+
+    def test_updates_game_log_when_player_one_has_taken_a_move_on_pot_3(self, mancala):
+        player_number = 1
+        pot_number = 3
+        mancala.play(player_number, pot_number)
+
+        assert mancala.game_log[0] == [[4, 4, 4, 4, 4, 4], [4, 4, 4, 4, 4, 4], [0, 0]]
+        assert mancala.game_log[1] == [[4, 4, 0, 5, 5, 5], [4, 4, 4, 4, 4, 4], [1, 0]]
