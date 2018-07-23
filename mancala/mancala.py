fddf8d7689d9eb5cf23ba52bdb5943eefac7d179
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2018-07-23 10:04:34 +0100

    Adds recursive sow function so other player can take a turn

--- mancala/mancala.py
+++ mancala/mancala.py
@@ -21,23 +21,34 @@ class Mancala():
         except IndexError as error:
             raise Exception("A valid pot must be selected for play") from error
 
-    def generate_turn(self, player_number, pot_number):
+    def sow(self, current_player, stones_to_sow, starting_pot, new_turn):
+        # sow a stone in all the pots on the current side until out of stones
+        for index, pot in enumerate(new_turn[current_player]):
+            if stones_to_sow > 0 and index >= starting_pot and index <= len(new_turn[current_player]):
+                new_turn[current_player][index] += 1
+                stones_to_sow -= 1
+
+        if stones_to_sow > 0:
+            # put a stone in the store
+            new_turn[2][current_player] += 1
+            stones_to_sow -= 1
+            # switch sides and continue
+            current_player = 0 if current_player == 1 else 0
+            self.sow(current_player, stones_to_sow, 0, new_turn)
+
+        print(new_turn)
+        return new_turn
+
+    def generate_turn(self, player_number, starting_pot):
         current_player = player_number
         new_turn = copy.deepcopy(self.game_log[-1])
-        stones_to_sow = copy.deepcopy(new_turn[current_player][pot_number]) #pick up stones
+        stones_to_sow = copy.deepcopy(new_turn[current_player][starting_pot])
 
-        new_turn[current_player][pot_number] = 0; #current pot is now empty
+        new_turn[current_player][starting_pot] = 0
 
-        while stones_to_sow:
-            pot_number += 1
-            while pot_number < len(new_turn[current_player]):
-                new_turn[current_player][pot_number] += 1
-                pot_number += 1
-                stones_to_sow -= 1
-            if stones_to_sow > 0:
-                new_turn[2][current_player] += 1
-                stones_to_sow -= 1
-        print(new_turn)
+        starting_pot += 1
+
+        self.sow(current_player, stones_to_sow, starting_pot, new_turn)
         return new_turn
 
     def play(self, player_number, pot_number):
