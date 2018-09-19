91fc916ac4a7ba5631fdd69e04213541621a6fa0
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2018-09-19 09:15:29 +0100

    Adds minimax player with alpha beta prurning

--- mancala/player.py
+++ mancala/player.py
@@ -31,6 +31,8 @@ class Player():
         last_move = game_instance.game_log[-1]
         winner = game_instance.winning_player if game_over else self.calculate_winner(last_move)
 
+        # Could you score this with difference between scores instead?
+
         if winner is self.player_number:
             print(f"Current player wins: {game_instance.game_log[-1]}")
             return 100 - depth
@@ -75,6 +77,64 @@ class Player():
             score_index = scores.index(min(scores))
             return [min(scores), moves[int(score_index)]]
 
+    def minimax_alpha_beta_score(self, game_instance, depth, game_over):
+        last_move = game_instance.game_log[-1]
+        winner = game_instance.winning_player if game_over else self.calculate_winner(last_move)
+
+        # Could you score this with difference between scores instead?
+
+        if winner is self.player_number:
+            print(f"Current player wins: {game_instance.game_log[-1]}")
+            return 100 - depth
+        elif winner is self.opposite_player_number:
+            print(f"Opposite player wins: {game_instance.game_log[-1]}")
+            return 0 - depth
+        else:
+            print(f"Game draw: {game_instance.game_log[-1]}")
+            return 50
+
+    def minimax_alpha_beta(self, game, phasing_player, alpha = float("-inf"), beta = float("inf"), best_move = None, depth = -1):
+        game_over = game.game_over()
+        if game_over or depth is self.maximum_depth:
+            return [self.minimax_alpha_beta_score(game, depth, game_over), best_move]
+
+        depth += 1
+
+        legal_moves = game.get_legal_moves(phasing_player)
+        last_move = game.game_log[-1]
+        next_player = self.get_opposite_player(phasing_player)
+
+        print(f"------Depth {depth} Player {phasing_player} Legal moves: {legal_moves}")
+
+        for move in legal_moves:
+            possible_game = MancalaBoard(6, 6, last_move)
+            take_another_turn = possible_game.play(phasing_player, move)
+
+            # See what the next player will do
+            if phasing_player == self.player_number: #is maximising node
+                result = self.minimax_alpha_beta(game, next_player, alpha, beta, move)[0]
+                if result > alpha:
+                    alpha = result
+                    best_move = move
+
+                if alpha >= beta: # Pruning
+                    break
+
+            else: #is minimising node
+                result = self.minimax_alpha_beta(game, next_player, alpha, beta, move)[0]
+                if result < beta:
+                    beta = result
+                    best_move = move
+
+                if beta <= alpha:  # Pruning
+                    break
+
+            best_score = alpha if phasing_player == self.player_number else beta
+            return [best_score, best_move]
+
+
+    def pick_minimax_alpha_beta(self):
+        return self.minimax_alpha_beta(self.mancala, self.player_number)[1]
 
     def pick_minimax(self):
         return self.minimax(self.mancala, self.player_number)[1]
@@ -87,6 +147,9 @@ class Player():
 
         if self.player_type == "minimax":
             pot = self.pick_minimax()
+
+        if self.player_type == "alphabeta":
+            pot = self.pick_minimax_alpha_beta()
 
         if self.player_type == "rightpot":
             pot = self.pick_right_pot()
