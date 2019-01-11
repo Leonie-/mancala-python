18a6cf10cc238446d6d0ae155d8662ebdb0cdc92
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-01-11 15:22:33 +0000

    Adds final state to game logs

--- mancala/generate_game_logs.py
+++ mancala/generate_game_logs.py
@@ -12,7 +12,7 @@ def init():
     first_player = 1
     player_types = ["minimax", "alphabeta", "random", "rightpot", "potwithleast", "potwithmost", "takeanotherturn", "avoidanotherturn"]
 
-    for game_number in range(5):
+    for game_number in range(10):
         mancala_board = MancalaBoard(pots, stones, None, game_number)
         player_one_type = random.choice(player_types)
         player_two_type = random.choice(player_types)
@@ -26,11 +26,18 @@ def init():
         game_logs = game.play(first_player)
         print(f"game_logs : {game_logs[-1]}")
 
+        game_final_result = {
+            "winner": mancala_board.winning_player,
+            "final_score_player_1": mancala_board.game_board_log[-1][2][0],
+            "final_score_player_2": mancala_board.game_board_log[-1][2][1],
+            "final_score_difference": abs(mancala_board.game_board_log[-1][2][0] - mancala_board.game_board_log[-1][2][1])
+        }
+
         with open('kalah_game_board_data.csv', 'a', newline='') as game_board_csv, open('kalah_game_state_data.csv', 'a', newline='') as game_state_csv:
             game_board_writer = csv.writer(game_board_csv)
             game_board_writer.writerow(game_logs[0])
 
             game_state_writer = csv.writer(game_state_csv)
-            game_state_writer.writerow(game_logs[1])
+            game_state_writer.writerow([game_final_result] + game_logs[1])
 
 init()
