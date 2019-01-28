58acf905e0be6db8b54bfd66d183e65c73c4444a
Leonie Kenyon <Leonie.Kenyon@bbc.co.uk>
2019-01-28 12:56:08 +0000

    Bugfixing for MCTS

--- mancala/mcts.py
+++ mancala/mcts.py
@@ -7,11 +7,10 @@ import player
 from game import Game
 
 class Node():
-    def __init__(self, game_state, move=None, parent=None, gets_another_turn=False):
-        self.player_number = game_state.current_player + 1
-        self.gets_another_turn = gets_another_turn
+    def __init__(self, game_state, player=None, move=None, parent=None):
+        self.game_state = game_state
+        self.player = player
         self.current_board_state = game_state.game_board_log[-1]
-        self.legal_moves = game_state.get_legal_moves(self.player_number)
         self.move = move
         self.parent_node = parent
         self.child_nodes = set()
@@ -19,10 +18,13 @@ class Node():
         self.number_of_visits = 0
         self.total_reward = 0
         self.is_fully_expanded = False
-        self.is_leaf = game_state.check_for_game_over(self.current_board_state)
+        self.is_leaf = game_state.game_is_over
 
     def check_child_moves_to_explore(self):
-        return set(self.legal_moves)^set(self.explored_child_moves)
+        legal_moves = self.game_state.get_legal_moves(self.player)
+        # print(f" legal moves: {legal_moves}")
+        # print(f" vs explored moves: {self.explored_child_moves}")
+        return set(legal_moves)^set(self.explored_child_moves)
 
     def add_explored_child_move(self, move):
         self.explored_child_moves.add(move)
@@ -31,17 +33,22 @@ class Node():
         self.child_nodes.add(child)
 
 class MCTS():
-    def __init__(self, mancala, time_limit_seconds, number_of_simulations=5, exploration_constant=1):
+    def __init__(self, mancala, player_number, time_limit_seconds, number_of_simulations=5, exploration_constant=1):
         self.mancala = mancala
         self.time_limit = time_limit_seconds
         self.number_of_simulations = number_of_simulations
         self.exploration_constant = exploration_constant
-        self.player = None
-
-    def pick_pot(self, player_number):
         self.player = player_number
-        # Create root node (top of tree)
-        root_node = Node(self.mancala)
+
+    def pick_pot(self):
+        # if self.mancala.current_player is 0:
+        #     self.mancala.check_player_turn_number(self.player)  # Ensure correct player number is set
+        # Create root node (top of tree) with correct starting player set
+        # next_player = self.get_next_player(self.player)
+        root_node = Node(self.mancala, self.player)
+        # root_node.game_state.switch_to_player(self.player)
+
+        # Set a time limit
         time_limit = time.time() + self.time_limit / 1000
         while time.time() < time_limit:
             self.selection(root_node)
@@ -66,29 +73,35 @@ class MCTS():
         moves_to_explore = parent_node.check_child_moves_to_explore()
         for child_move in moves_to_explore:
             # Add a new node to the tree (expansion)
-            node = self.play_move_and_add_node(parent_node, child_move)
-            # Run simulations of random moves from this node until a leaf is reached (simulation)
-            for index in range(self.number_of_simulations):
-                reward = self.run_simulation(node.current_board_state, node.player_number)
+            node = self.play_move_and_create_node(parent_node, child_move)
+            parent_node.add_child_node(node)
+            parent_node.add_explored_child_move(child_move)
+
+            if node.is_leaf:
+                reward = self.get_reward(node.game_state.winning_player)
                 # Backpropagate and update the nodes with relevant stats (update)
                 self.backpropogate(node, reward)
+            else:
+                # Run simulations of random moves from this node until a leaf is reached (simulation)
+                for index in range(self.number_of_simulations):
+                    reward = self.run_simulation(node.current_board_state, node.player)
+                    # Backpropagate and update the nodes with relevant stats for each simulation (update)
+                    self.backpropogate(node, reward)
 
         parent_node.is_fully_expanded = True
         return parent_node
 
-    def play_move_and_add_node(self, parent_node, move):
-        player = self.get_opposite_player(parent_node)
+    def play_move_and_create_node(self, parent_node, move):
         mancala_board = MancalaBoard(6, 6, parent_node.current_board_state)
-        gets_another_turn = mancala_board.play(player, move)
-        node = Node(mancala_board, move, parent_node, gets_another_turn)
+        extra_turn = mancala_board.play(parent_node.player, move)
+        next_player = self.get_next_player(parent_node.player, extra_turn)
         # Add new node to the tree
-        parent_node.add_child_node(node)
-        return node
+        return Node(mancala_board, next_player, move, parent_node)
 
-    def get_opposite_player(self, parent_node):
-        if parent_node.gets_another_turn is True:
-            return parent_node.player_number
-        return 1 if parent_node.player_number is 2 else 2
+    def get_next_player(self, current_player, extra_turn):
+        if extra_turn is True:
+            return current_player
+        return 1 if current_player is 2 else 2
 
     def run_simulation(self, board_state, player_number, max_depth=5):
         mancala_board = MancalaBoard(6, 6, board_state)
@@ -104,7 +117,7 @@ class MCTS():
         return self.get_reward(winner)
 
     def get_reward(self, winner):
-        if winner is self.player_number:
+        if winner is self.player:
             return 1
         if winner is "draw":
             return 0.5
